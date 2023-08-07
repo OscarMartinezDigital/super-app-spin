153f29074e45a2bb5860586eab08691f875cb77d
Oscar Martinez <oscar.martinez@digitalfemsa.com>
2023-08-07 15:14:45 -0600

    Main Navigation Added

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import MainNavigation from './src/mainNavigation';
 import ThemeProvider from './src/theme/ThemeProvider';
 import {Button} from './src';
 
 const App = () => {
   return (
     <ThemeProvider>
-      <SafeAreaView>
-        <Button text="Hola ironhackers" onPress={() => console.log('spin')} />
+      <SafeAreaView style={{flex: 1}}>
+        <NavigationContainer>
+          <MainNavigation />
+        </NavigationContainer>
       </SafeAreaView>
     </ThemeProvider>
   );
---
+++ src/mainNavigation/types.ts
@@ -0,0 +1,6 @@
+export type TabParamList = {
+  Home: undefined;
+  Beneficios: undefined;
+  Cartera: undefined;
+  Cuenta: undefined;
+};
