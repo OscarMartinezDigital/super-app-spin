9b5192ec2704a53850d99f43e39cfd80061ae54f
José De Ita <jose.deita@digitalfemsa.com>
2023-08-08 18:08:35 -0500

    Fix typos in navigation and update types

--- src/mainNavigation/types.ts
+++ src/mainNavigation/types.ts
@@ -1,12 +1,23 @@
+import {StackScreenProps, StackNavigationProp} from '@react-navigation/stack';
+
 export type TabParamList = {
   Home: undefined;
-  Beneficios: undefined;
-  Cartera: undefined;
-  Cuenta: undefined;
+  Benefits: undefined;
+  Wallet: undefined;
+  Account: undefined;
 };
 
 export type StackParamList = {
   MainTabs: TabParamList;
   Movements: undefined;
+  TransactionDetail: {id: number};
   ChangePoints: undefined;
 };
+
+export type BenefitsProps = StackScreenProps<TabParamList, 'Benefits'>;
+
+export type TransactionDetailProps = StackScreenProps<
+  StackParamList,
+  'TransactionDetail'
+>;
+export type NavigationProps = StackNavigationProp<StackParamList>;
