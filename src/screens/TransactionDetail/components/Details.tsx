599cd0e1a33ab265140578200f189822411caea0
José De Ita <jose.deita@digitalfemsa.com>
2023-08-08 18:09:53 -0500

    Added transaction detail screen

---
+++ src/screens/TransactionDetail/components/Details.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+type DetailsProps = {
+  points: number;
+  date: string;
+};
+
+export default function Details({points, date}: DetailsProps) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <Text style={[styles.baseText, styles.itemKey]}>Monto total:</Text>
+        <Text style={[styles.baseText, styles.itemValue]}>$30.00</Text>
+      </View>
+      <View style={styles.itemContainer}>
+        <Text style={[styles.baseText, styles.itemKey]}>Fecha:</Text>
+        <Text style={[styles.baseText, styles.itemValue]}>21/5/2023</Text>
+      </View>
+      <View style={styles.itemContainer}>
+        <Text style={[styles.baseText, styles.itemKey]}>Úsalos desde el:</Text>
+        <Text style={[styles.baseText, styles.itemValue]}>21/8/2023</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 25,
+    borderBottomWidth: 1,
+    borderColor: '#E6E6EC',
+    gap: 8,
+    paddingBottom: 16,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 32,
+  },
+  baseText: {
+    color: '#373764',
+    fontFamily: 'Inter',
+    fontSize: 16,
+    lineHeight: 16,
+    letterSpacing: -0.32,
+  },
+  itemKey: {
+    fontWeight: '400',
+  },
+  itemValue: {
+    fontWeight: '600',
+  },
+});
---
+++ src/screens/TransactionDetail/components/Header.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+import Card from '../../../components/Card/Card';
+
+type HeaderDetailProps = {
+  entityName: string;
+  points: number;
+  operation: 'earned' | 'used' | string;
+};
+
+const image = require('../../../assets/logos/volaris.png');
+
+export default function Header({
+  entityName,
+  points,
+  operation,
+}: HeaderDetailProps) {
+  return (
+    <View style={styles.header}>
+      <Card style={styles.cardContainer}>
+        <View style={styles.card}>
+          <Text style={styles.title}>Volaris</Text>
+          <View style={styles.body}>
+            <Text style={styles.textBody}>En esta compra ganaste:</Text>
+          </View>
+
+          <View style={styles.pointsContainer}>
+            <Text style={styles.sign}>
+              {operation === 'earned' ? '+' : '-'}
+            </Text>
+            <Text style={styles.points}>{points}</Text>
+          </View>
+        </View>
+      </Card>
+      <Image source={image} style={styles.logo} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 16,
+  },
+  header: {
+    alignItems: 'center',
+  },
+  logo: {
+    height: 72,
+    width: 72,
+    position: 'absolute',
+  },
+  cardContainer: {
+    width: '100%',
+    marginTop: 42,
+  },
+  card: {
+    paddingTop: 38,
+    alignItems: 'center',
+  },
+
+  title: {
+    color: '#05053D',
+    fontFamily: 'Poppins',
+    fontSize: 20,
+    fontWeight: '500',
+    lineHeight: 24,
+  },
+  body: {
+    width: 280,
+    padding: 10,
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F7',
+    marginVertical: 8,
+  },
+  textBody: {
+    color: '#05053D',
+    fontFamily: 'Inter',
+    fontSize: 16,
+    fontWeight: '400',
+    lineHeight: 20,
+    letterSpacing: -0.32,
+  },
+  pointsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: 10,
+  },
+  sign: {
+    color: '#1723D3',
+    fontFamily: 'Poppins',
+    fontSize: 24,
+    fontWeight: '700',
+    lineHeight: 32,
+  },
+  points: {
+    color: '#05053D',
+    fontFamily: 'Poppins',
+    fontSize: 40,
+    fontWeight: '500',
+    lineHeight: 52,
+    marginLeft: 4,
+  },
+});
