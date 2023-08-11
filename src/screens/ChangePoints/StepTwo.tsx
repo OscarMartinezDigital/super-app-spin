726a772f51fa383c5e2a1d5cd8154635932094e7
José De Ita <jose.deita@digitalfemsa.com>
2023-08-11 11:51:48 -0600

    Justify chips on change points step two

--- src/screens/ChangePoints/StepTwo.tsx
+++ src/screens/ChangePoints/StepTwo.tsx
@@ -110,6 +110,7 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     letterSpacing: -0.32,
     marginBottom: 16,
+    textAlign: 'justify',
   },
   message: {
     color: '#69698B',
@@ -128,9 +129,9 @@ const styles = StyleSheet.create({
     gap: 16,
   },
   pairChipsContainer: {
+    width: '100%',
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    gap: 16,
+    justifyContent: 'space-around',
   },
 });
 
